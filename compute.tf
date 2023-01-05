57125e5bd02a109f8d10e83c21b31f9dcad3a525
Michael Chavez <mchavez@space-rocket.com>
2023-01-05 10:22:32 +0000

    running my_app

--- compute.tf
+++ compute.tf
@@ -4,8 +4,7 @@ locals {
     HOST         = var.main_domain_name
     PORT         = "8080"
     PHX_PORT     = "8080"
-    # DATABASE_URL = join("", ["ecto://", aws_db_instance.sre_db.username, ":", var.db_password, "@", aws_db_instance.sre_db.address, ":", aws_db_instance.sre_db.port, "/", var.db_name])
-    DATABASE_URL = ""
+    DATABASE_URL = join("", ["ecto://", aws_db_instance.sre_db.username, ":", var.db_password, "@", aws_db_instance.sre_db.address, ":", aws_db_instance.sre_db.port, "/", var.db_name])
   })
 }
 
@@ -31,40 +30,39 @@ resource "aws_key_pair" "sre_auth" {
 }
 
 # RDS DB
-# resource "aws_db_parameter_group" "sre_db_parameter_group" {
-#   name   = "sre-db-parameter-group"
-#   family = "postgres13"
+resource "aws_db_parameter_group" "sre_db_parameter_group" {
+  name   = "sre-db-parameter-group"
+  family = "postgres13"
 
-#   parameter {
-#     name  = "log_connections"
-#     value = "1"
-#   }
-# }
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+}
 
-# resource "aws_db_instance" "sre_db" {
-#   identifier             = "sre-db"
-#   instance_class         = "db.t3.micro"
-#   allocated_storage      = 5
-#   engine                 = "postgres"
-#   engine_version         = "13.8"
-#   db_name                = var.db_name
-#   username               = var.db_user
-#   password               = var.db_password
-#   db_subnet_group_name   = aws_db_subnet_group.sre_db_subnet_group.name
-#   vpc_security_group_ids = [aws_security_group.sre_db_security_group.id]
-#   parameter_group_name   = aws_db_parameter_group.sre_db_parameter_group.name
-#   publicly_accessible    = true
-#   skip_final_snapshot    = true
-# }
+resource "aws_db_instance" "sre_db" {
+  identifier             = "sre-db"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "13.8"
+  db_name                = var.db_name
+  username               = var.db_user
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.sre_db_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.sre_db_security_group.id]
+  parameter_group_name   = aws_db_parameter_group.sre_db_parameter_group.name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+}
 
 # EC2
 resource "aws_instance" "sre_main" {
-  # depends_on             = [aws_db_instance.sre_db]
+  depends_on             = [aws_db_instance.sre_db]
   count                  = var.main_instance_count
   instance_type          = var.main_instance_type
   ami                    = data.aws_ami.server_ami.id
   key_name               = aws_key_pair.sre_auth.id
-  # vpc_security_group_ids = [aws_security_group.sre_sg.id]
   vpc_security_group_ids = [aws_security_group.sre_app_sg.id]
   subnet_id              = aws_subnet.sre_public_subnet[count.index].id
 
@@ -97,8 +95,6 @@ resource "null_resource" "ssh" {
       host        = aws_instance.sre_main[count.index].public_ip
     }
   }
-
-  # depends_on = [aws_eip_association.sre_eip_assoc]
 }
 
 resource "null_resource" "main-playbook" {
@@ -112,25 +108,25 @@ resource "null_resource" "main-playbook" {
   depends_on = [null_resource.ssh]
 }
 
-# output "RDS-Endpoint" {
-#   value = aws_db_instance.sre_db.endpoint
-# }
+output "RDS-Endpoint" {
+  value = aws_db_instance.sre_db.endpoint
+}
 
-# output "RDS_HOSTNAME" {
-#   description = "RDS instance hostname"
-#   value       = aws_db_instance.sre_db.address
-#   sensitive   = true
-# }
+output "RDS_HOSTNAME" {
+  description = "RDS instance hostname"
+  value       = aws_db_instance.sre_db.address
+  sensitive   = true
+}
 
-# output "rds_port" {
-#   description = "RDS instance port"
-#   value       = aws_db_instance.sre_db.port
-# }
+output "rds_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.sre_db.port
+}
 
-# output "rds_username" {
-#   description = "RDS instance root username"
-#   value       = aws_db_instance.sre_db.username
-# }
+output "rds_username" {
+  description = "RDS instance root username"
+  value       = aws_db_instance.sre_db.username
+}
 
 output "rds_password" {
   description = "RDS password"
